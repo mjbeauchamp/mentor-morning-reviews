155f83241bf79c49fb1d274387c6bf5f0fcfaeae
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-10-14 19:55:55 -0600

    Basic house point functionality working

--- house-point-score/src/App.js
+++ house-point-score/src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import House from "./components/House"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      gryffindor: 0,
+      hufflepuff: 0,
+      ravenclaw: 0,
+      slytherin: 0
+    }
+  }
+
+  pointGryffindor = (num) => {
+    this.setState({
+      gryffindor: this.state.gryffindor += num
+    })
+  }
+
+  pointHufflepuff = (num) => {
+    this.setState({
+      hufflepuff: this.state.hufflepuff += num
+    })
+  }
+
+  pointRavenclaw = (num) => {
+    this.setState({
+      ravenclaw: this.state.ravenclaw += num
+    })
+  }
+
+  pointSlytherin = (num) => {
+    this.setState({
+      slytherin: this.state.slytherin += num
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>House Scores</h1>
+        <div className="houses">
+          <House addPoints={this.pointGryffindor} house="Gryffindor" points={this.state.gryffindor}/>
+          <House addPoints={this.pointHufflepuff} house="Hufflepuff" points={this.state.hufflepuff}/>
+          <House addPoints={this.pointRavenclaw} house="Ravenclaw" points={this.state.ravenclaw}/>
+          <House addPoints={this.pointSlytherin} house="Slytherin" points={this.state.slytherin}/>
+        </div>
       </div>
     );
   }
---
+++ house-point-score/src/components/House.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class House extends Component {
+    render(){
+        return (
+            <div className="house">
+                <h2>{this.props.house}</h2>
+                <h3>{this.props.points}</h3>
+                <button onClick={() => this.props.addPoints(10)}>10 Points</button>
+                <button onClick={() => this.props.addPoints(20)}>20 Points</button>
+                <button onClick={() => this.props.addPoints(50)}>50 points</button>
+            </div>
+            
+        )
+    }
+}
+
+export default House
